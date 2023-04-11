f5879f6bb1c699222b4856f57c9606c472d1a305
Sophia Contesini <sophiamarques214@gmail.com>
2023-04-11 20:05:16 -0300

    feat: create Header in page

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function Header() {
+  return (
+    <header>
+      <Typography
+        variant='h2'
+        color= 'secondary'
+        mt={4}
+        align='center'
+      >
+        Colab Users List
+      </Typography>
+    </header>
+  );
+}
