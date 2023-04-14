f691b3f4a43587469cc13628c91d4056434d1924
Sophia Contesini <sophiamarques214@gmail.com>
2023-04-14 13:55:53 -0300

    fix: loading bug

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,39 +1,46 @@
 /* eslint-disable react/jsx-no-undef */
 import { Grid } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { act } from 'react-dom/test-utils';
 import getUsers from '../services/fetchApi';
 import Cards from './Cards';
 
 
 export function Users() {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getUsers(10).then(users => setUsers(users));
-  }, []);
-
-
-  const loading = <span>Loading</span>
+ useEffect(() => {
+   setIsLoading(true);
+   getUsers(12).then(users => {
+     act(() => {
+       setUsers(users);
+       setIsLoading(false);
+     });
+   });
+ }, []);
 
   return (
     <div>
-      <Grid container>
-        {!users
-          ? loading
-          : users.map((user, index) => (
-              <Grid
-                item
-                alignItems='center'
-                key={index}
-                xs={12}
-                sm={6}
-                md={4}
-               lg={3} 
-              >
-                <Cards userData={user} />
-              </Grid>
-            ))}
-      </Grid>
+      {isLoading
+        ? <span>Loading users...</span>
+        : <Grid container alignItems='center'>
+            {users?.length > 0
+              ? users.map((user, index) => (
+                  <Grid
+                    item
+                    alignItems='center'
+                    key={index}
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    lg={3}
+                  >
+                    <Cards userData={user} />
+                  </Grid>
+                ))
+              : <span>No users found.</span>}
+          </Grid>}
     </div>
   );
 }
